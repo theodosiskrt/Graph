390b1b828342308fd8c19d84acc9aa4d0e82581a
theodosis <karataristheodosis@gmail.com>
2021-11-29 16:01:22 +0200

    Add error text for the button

--- src/components/ShowPlotButton.js
+++ src/components/ShowPlotButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Button } from "@mui/material";
+import { Button, Typography, Grid } from "@mui/material";
 
 import { setPlotData } from "../store/actions";
 
 const ShowPlotButton = () => {
   const selectedNames = useSelector((state) => state.selectedData.names);
+  const selectedYear = useSelector((state) => state.selectedData.year);
 
   const dispatch = useDispatch();
 
@@ -21,10 +22,35 @@ const ShowPlotButton = () => {
     const data = await res.json();
     dispatch(setPlotData(data.map((data) => data.x)));
   };
+
+  const errorText = !selectedNames.length
+    ? "Select at least 1 company name."
+    : !selectedYear
+    ? "Select a year."
+    : false;
+
   return (
-    <Button onClick={fetchAndSetPlotData} variant="contained" fullWidth>
-      Show Plot
-    </Button>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography
+          variant="subtitle2"
+          align="center"
+          style={{ color: "#aaa" }}
+        >
+          {errorText}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          onClick={fetchAndSetPlotData}
+          variant="contained"
+          fullWidth
+          disabled={errorText}
+        >
+          Show Plot
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
